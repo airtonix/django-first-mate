6d7e81493c1504cb5dd71b07ab2bc09e130f3df5
Zenobius Jiricek <airtonix@gmail.com>
2013-11-11 19:27:13 +1030

    remove switch

--- first_mate/management/commands/runserver.py
+++ first_mate/management/commands/runserver.py
@@ -4,14 +4,13 @@ import subprocess
 import atexit
 import signal
 
-from optparse import make_option
-
 from django.conf import settings
 from django.core.management.commands.runserver import Command as RunserverCommand
 
 
 MESSAGES = {
     "Init": ">>> Loading...",
+    "MissingConfig": ">>> You need to provide an absolute path to the folder containing config.rb. i.e.: FIRSTMATE_COMPASS_PROJECT_PATH.",
     "NotFound": ">>> Could not find a config.rb at {path}. Perhaps run : compass config",
     "StartingFor": ">>> Starting the compass watch command for path: {path}",
     "StartingPid": ">>> Compass watch process on pid: {pid}",
@@ -20,10 +19,7 @@ MESSAGES = {
 
 
 class Command(RunserverCommand):
-    option_list = RunserverCommand.option_list + (
-        make_option('--compass', dest='compass_project_path', default=None,
-                    help='Specifies the project directory for a compass project'),
-    )
+    option_list = RunserverCommand.option_list
 
     def run(self, *args, **options):
         """Runs the server and the compass watch process"""
@@ -32,17 +28,21 @@ class Command(RunserverCommand):
         if use_reloader and os.environ.get("RUN_MAIN") != "true":
             self.stdout.write(self.style.NOTICE(MESSAGES.get("Init")))
 
-            project_path = options.get('compass_project_path')
+            project_path = getattr(
+                settings, 'FIRSTMATE_COMPASS_PROJECT_PATH', None)
 
             if not project_path:
-                project_path = getattr(
-                    settings, 'FIRSTMATE_COMPASS_PROJECT_PATH', None)
-
-            if project_path and not os.path.exists(os.path.join(project_path, 'config.rb')):
-                self.stdout.write(MESSAGES.get("NotFound").format(path=project_path))
+                self.stdout.write(
+                    MESSAGES.get("NotFound").format(path=project_path))
                 sys.exit(1)
 
-            self.stdout.write(self.style.NOTICE(MESSAGES.get("StartingFor").format(path=project_path)) + "\n")
+            if project_path and not os.path.exists(os.path.join(project_path, 'config.rb')):
+                self.stdout.write(
+                    MESSAGES.get("NotFound").format(path=project_path))
+                sys.exit(1)
+
+            self.stdout.write(
+                self.style.NOTICE(MESSAGES.get("StartingFor").format(path=project_path)) + "\n")
             self.start_compass_watch(project_path)
 
         super(Command, self).run(*args, **options)
@@ -55,7 +55,8 @@ class Command(RunserverCommand):
             stdout=self.stdout,
             stderr=self.stderr,
         )
-        self.stdout.write(self.style.NOTICE(MESSAGES.get("StartingPid").format(pid=self.compass_process.pid)) + "\n")
+        self.stdout.write(
+            self.style.NOTICE(MESSAGES.get("StartingPid").format(pid=self.compass_process.pid)) + "\n")
 
         def kill_compass_project(pid):
             self.stdout.write(
